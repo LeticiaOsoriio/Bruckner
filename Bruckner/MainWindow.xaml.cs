200d0db1284369e064078246c6d318ca3a2b6cbc
Juan Elias Romero Sanchez <jers.sist@gmail.com>
2024-09-13 21:26:26 -0600

    Ajuste de todo

--- Bruckner/MainWindow.xaml.cs
+++ Bruckner/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
 
 namespace Bruckner
 {
@@ -16,9 +17,69 @@ namespace Bruckner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer mediaPlayer;
+        private string[] audioFiles;
+        private int currentFileIndex = 0;
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+            this.ResizeMode = ResizeMode.NoResize;
+            // this.Topmost = true;
+
+            //mediaPlayer = new MediaPlayer();
+
+            // Reproducir el archivo menu.mp3 al iniciar la aplicación
+            //PlayMenuAudio();
+            //FamilyChildhoodItem.Selected += TreeViewItem_Selected;
+
+
+            SessionManager.CurrentLanguage = "es-ES";
+            LoadLanguage(SessionManager.CurrentLanguage);
+
+            Tit2.Selected += TreeViewItem_Selected;
+            Tit3.Selected += TreeViewItem_Selected;
+            Tit4.Selected += TreeViewItem_Selected;
+            Tit5.Selected += TreeViewItem_Selected;
+            Tit7.Selected += TreeViewItem_Selected;
+            Tit8.Selected += TreeViewItem_Selected;
+            Tit9.Selected += TreeViewItem_Selected;
+            Tit10.Selected += TreeViewItem_Selected;
+            Tit11.Selected += TreeViewItem_Selected;
+            Tit12.Selected += TreeViewItem_Selected;
+           
+            Tit14.Selected += TreeViewItem_Selected;
+            Tit15.Selected += TreeViewItem_Selected;
+            Tit16.Selected += TreeViewItem_Selected;
+            Tit17.Selected += TreeViewItem_Selected;
+            //Tit18.Selected += TreeViewItem_Selected;
+            //Tit19.Selected += TreeViewItem_Selected;
+            //Tit20.Selected += TreeViewItem_Selected;
+        }
+        private void LoadLanguage(string cultureCode)
+        {
+            var cultureInfo = new CultureInfo(cultureCode);
+
+            // Cambiar el idioma del hilo actual
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            // Limpiar los diccionarios de recursos existentes
+
+            // Cargar el diccionario de recursos correspondiente al idioma seleccionado
+            var dict = new ResourceDictionary();
+            switch (cultureCode)
+            {
+                case "es-ES":
+                    dict.Source = new Uri("Recursos\\idioma\\MenuEs.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dict.Source = new Uri("Recursos\\idioma\\MenuIn.xaml", UriKind.Relative);
+                    break;
+            }
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(dict);
         }
         private void MenuListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -28,6 +89,81 @@ namespace Bruckner
                 string pageName = selectedItem.Tag.ToString();
                 ContentFrame.Navigate(new Uri($"Vistas/{pageName}.xaml", UriKind.Relative));
             }
+        }
+
+        private void PlayMenuAudio()
+        {
+            // Ruta del recurso menu.mp3 en el proyecto
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Ruta completa al archivo menu.mp3 (asumiendo que está en Recursos/audio/Spanisch relativo al ejecutable)
+            string audioFilePath = Path.Combine(exeDirectory, @"Recursos\menu.mp3");
+
+            // Cargar y reproducir el archivo de audio
+            mediaPlayer.Open(new Uri(audioFilePath, UriKind.Absolute));
+            mediaPlayer.Play();
+        }
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem selectedItem = e.OriginalSource as TreeViewItem;
+
+            if (selectedItem != null)
+            {
+                // Obtener el valor del Tag
+                string tagValue = selectedItem.Tag as string;
+
+                if (!string.IsNullOrEmpty(tagValue))
+                {
+                    // Llamar al método de manejo de selección
+                    goNavi(tagValue);
+                }
+
+                // Evitar que el evento se propague a otros elementos
+                e.Handled = true;
+            }
+        }
+        public void goNavi(string tag)
+        {
+           // mediaPlayer.Stop();
+
+            this.Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Recursos/Img/Hintergrund.jpg")));
+            string pageName = tag;
+            ContentFrame.Navigate(new Uri($"Vistas/{pageName}.xaml", UriKind.Relative));
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.Key == Key.F12)
+            {
+                // Mostrar el mensaje de confirmación
+                MessageBoxResult resultado = MessageBox.Show(
+                    "¿Está seguro de que desea salir?", // Mensaje del diálogo
+                    "Confirmar salida",                 // Título del diálogo
+                    MessageBoxButton.YesNo,             // Botones de Sí y No
+                    MessageBoxImage.Question);          // Icono de pregunta
+
+                // Si el usuario selecciona "Sí", cerrar la aplicación
+                if (resultado == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void ChangeLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            if (SessionManager.CurrentLanguage == "es-ES")
+            {
+                SessionManager.CurrentLanguage = "en-US";
+            }
+            else
+            {
+                SessionManager.ChangeLanguage("es-ES");
+            }
+            LoadLanguage(SessionManager.CurrentLanguage);
+            ContentFrame.Content = null;
+            this.Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Recursos/Img/saver.jpg")));
         }
     }
 
--- Bruckner/Vistas/Biografia1.xaml.cs
+++ Bruckner/Vistas/Biografia1.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,6 +41,35 @@ namespace Bruckner.Vistas
         public Biografia1()
         {
             InitializeComponent();
+            LoadLanguage(SessionManager.CurrentLanguage);
+        }
+
+
+        private void LoadLanguage(string cultureCode)
+        {
+            // Crear nueva cultura
+            var cultureInfo = new CultureInfo(cultureCode);
+
+            // Cambiar el idioma del hilo actual
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            // Limpiar los diccionarios de recursos existentes
+
+            // Cargar el diccionario de recursos correspondiente al idioma seleccionado
+            var dict = new ResourceDictionary();
+            switch (cultureCode)
+            {
+                case "es-ES":
+                    dict.Source = new Uri("..\\Recursos\\idioma\\BiografiaEs.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dict.Source = new Uri("..\\Recursos\\idioma\\BiografiaIn.xaml", UriKind.Relative);
+                    break;
+            }
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(dict);
+
         }
     }
 }
